fe22c503fc5280b0637267e121e8fa2d0d7c13dc
HP <munaramamadalieva62@gmail.com>
2023-07-12 12:20:21 +0600

    second commit

---
+++ src/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    private String name;
+    private String surname;
+    private int age;
+ public Student(String name,String surname,int age){
+     this.name=name;
+     this.surname=surname;
+     this.age=age;
+ }
+
+
+
+}
